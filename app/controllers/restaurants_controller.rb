c893cfbb5dce9dde96ae90e4042e7c510a208309
Andre Pires <andrecabralpires@gmail.com>
2018-03-21 14:47:09 +0000

    lecture 1

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,23 @@
+class RestaurantsController < ApplicationController
+  # Let's fake a database records
+  RESTAURANTS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+
+  def index
+    @category = params[:food_type]
+    @restaurants = RESTAURANTS.select { |id, r| r[:category] == @category }
+  end
+
+  def create
+
+    puts "Add to DB restaurant '#{params[:name]}' with address '#{params[:address]}'"
+  end
+
+  def show
+    @restaurant = RESTAURANTS[params[:id].to_i]
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
+  get 'about', to: 'pages#about', as: :about
+
+  get 'contact', to: 'pages#contact', as: :contact
+
+  get 'restaurants', to: 'restaurants#index'
+
+  post 'restaurants', to: 'restaurants#create'
+
+  get 'restaurants/:id', to:  'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
